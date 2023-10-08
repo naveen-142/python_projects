d6dc32432e209986664f734b65c9cd5a726e4a1c
Naveen kumar <147265299+naveen-142@users.noreply.github.com>
2023-10-08 13:48:15 +0530

    Add files via upload

--- to_do_list.py
+++ to_do_list.py
@@ -8,12 +8,16 @@ class ToDoList:
         self.entry = Entry(self.root)
         self.addButton = Button(self.root, text="Add Task", command=self.add_task)
         self.delButton = Button(self.root, text="Delete Task", command=self.delete_task)
+        self.updateButton = Button(self.root, text="Update Task", command=self.update_task)
+        self.clearButton = Button(self.root, text="Clear List", command=self.clear_list)
 
         # GUI Layout
         self.entry.pack()
         self.addButton.pack()
         self.listbox.pack()
         self.delButton.pack()
+        self.updateButton.pack()
+        self.clearButton.pack()
 
     def add_task(self):
         task = self.entry.get()
@@ -25,8 +29,22 @@ class ToDoList:
         try:
             task_index = self.listbox.curselection()[0]
             self.listbox.delete(task_index)
-        except:
+        except IndexError:
             pass
+
+    def update_task(self):
+        try:
+            task_index = self.listbox.curselection()[0]
+            new_task = self.entry.get()
+            if new_task != "":
+                self.listbox.delete(task_index)
+                self.listbox.insert(task_index, new_task)
+                self.entry.delete(0, END)
+        except IndexError:
+            pass
+
+    def clear_list(self):
+        self.listbox.delete(0, END)
 
 root = Tk()
 root.title("Python To-Do List")
