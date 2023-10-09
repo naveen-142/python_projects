8b50f562d43ecd707dab39b4d7c50e084464e6b3
Naveen kumar <147265299+naveen-142@users.noreply.github.com>
2023-10-09 21:30:34 +0530

    Update password_gen.py

--- password_gen.py
+++ password_gen.py
@@ -21,25 +21,20 @@ class Window():
         self.master.title("Password Generator")
         self.master.geometry("580x250")
 
-        self.ptype = tkinter.StringVar(self.master,
-                                       value=Window.CHARS_OPTIONS[0])
-        self.n_chars = tkinter.IntVar(self.master,
-                                      value=Window.MIN_CHARS)
+        self.ptype = tkinter.StringVar(self.master,value=Window.CHARS_OPTIONS[0])
+        self.n_chars = tkinter.IntVar(self.master,value=Window.MIN_CHARS)
 
-        self.label_chars = tkinter.Label(self.master,
-                                         text="Chars that will compose the Password: ")
+        self.label_chars = tkinter.Label(self.master,text="Chars that will compose the Password: ")
         self.option_menu_chars = tkinter.OptionMenu(self.master, self.ptype, *Window.CHARS_OPTIONS)
 
         self.frame_n_chars = tkinter.Frame(self.master)
         self.label_num_chars = tkinter.Label(self.master, text="Password Length:")
-        self.option_menu_n_chars = tkinter.OptionMenu(self.master, self.n_chars,
-                                                      *range(Window.MIN_CHARS, Window.MAX_CHARS + 1))
+        self.option_menu_n_chars = tkinter.OptionMenu(self.master, self.n_chars,*range(Window.MIN_CHARS, Window.MAX_CHARS + 1))
 
         self.text_password_out = tkinter.Text(self.master, border=2, height=2, width=30)
 
         self.frame_buttons = tkinter.Frame()
-        self.button_generate = tkinter.Button(self.frame_buttons, text="Generate", width=8,
-                                              command=lambda: self.set_password())
+        self.button_generate = tkinter.Button(self.frame_buttons, text="Generate", width=8,command=lambda: self.set_password())
         self.button_close = tkinter.Button(self.frame_buttons, text="Close", command=self.master.quit, width=8)
 
         self.label_chars.grid(row=0, column=0, pady=Window.GRID_PADY)
@@ -75,4 +70,3 @@ class Window():
 
 if __name__ == "__main__":
     Window()
-
